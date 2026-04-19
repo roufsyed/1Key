b7e1f44589d2ed97f3145b3c7a191b43e02512ab
roufsyed <afrid704@gmail.com>
2026-04-19 19:41:43 +0530

    Add 3 days, 1 week, and 3 weeks recheck interval options

--- app/src/main/java/com/onekey/core/domain/model/MasterPasswordInterval.kt
+++ app/src/main/java/com/onekey/core/domain/model/MasterPasswordInterval.kt
@@ -2,7 +2,9 @@ package com.onekey.core.domain.model
 
 enum class MasterPasswordInterval(val hours: Int, val label: String) {
     HOURS_48(48, "48 hours"),
-    HOURS_72(72, "72 hours");
+    HOURS_72(72, "3 days"),
+    WEEK_1(168, "1 week"),
+    WEEKS_3(504, "3 weeks");
 
     val millis: Long get() = hours * 3_600_000L
 }
