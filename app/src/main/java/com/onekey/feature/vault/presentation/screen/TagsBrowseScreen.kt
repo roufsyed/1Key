a3198b79d3f24a327e14a95f082928c4890b8203
roufsyed <afrid704@gmail.com>
2026-04-19 14:14:05 +0530

    Fix missing dp import in TagsBrowseScreen

--- app/src/main/java/com/onekey/feature/vault/presentation/screen/TagsBrowseScreen.kt
+++ app/src/main/java/com/onekey/feature/vault/presentation/screen/TagsBrowseScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.onekey.feature.vault.presentation.viewmodel.VaultViewModel
