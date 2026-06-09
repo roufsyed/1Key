770499b182d634f85249b52fe8b19ca44292007a
roufsyed <afrid704@gmail.com>
2026-06-09 16:53:59 +0530

    Bump version name and version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.onekey"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.2.0"
     }
 
     buildTypes {
